5e176632a887e7098a6e12afa5b01d6172bb1217
Ielle <kjssd@163.com>
2017-05-05 12:46:40 +0800

    'three'

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.coolweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     private  String[] data =
             {"北京","上海","天津","重庆","香港",
@@ -21,9 +25,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 MainActivity.this,android.R.layout.simple_list_item_1,data);
-        ListView listView = (ListView) findViewById(R.id.province);
+        ListView listView = (ListView) findViewById(R.id.province_list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,data[position],Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,CityActivity.class);
+                intent.putExtra("province",data[position]);
+                startActivity(intent);
+
+            }
+        });
     }
 }
