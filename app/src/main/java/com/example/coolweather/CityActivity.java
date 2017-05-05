1cf68c7bf35cee9383f7ef537de7e8636a9f1d69
Ielle <kjssd@163.com>
2017-05-05 13:41:38 +0800

    'Four'

--- app/src/main/java/com/example/coolweather/CityActivity.java
+++ app/src/main/java/com/example/coolweather/CityActivity.java
@@ -2,13 +2,39 @@ package com.example.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CityActivity extends AppCompatActivity {
+    private String[] city01 = {"北京"};
+    private String[] city02 = {"上海"};
+    private String[] city03 = {"天津"};
+    private String[] city04 = {"重庆"};
+    private String[] city17 = {"杭州","温州","嘉兴","宁波","绍兴","台州","湖州","丽水","金华","衢州","舟山"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String province = getIntent().getStringExtra("province");
+        String[] data=null;
+        if ("北京".equals(province)){
+            data=city01;
+        }else if ("上海".equals(province)){
+            data=city02;
+        }else if ("天津".equals(province)){
+            data=city03;
+        }else if ("重庆".equals(province)){
+            data=city04;
+        }else {
+            data=city17;
+        }
+
+
+        ListView listView = (ListView) findViewById(R.id.province_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                CityActivity.this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
 
     }
 }
