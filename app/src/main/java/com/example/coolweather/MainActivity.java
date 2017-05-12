715c733a1d67eb47a073f9d7e1a408ecb9651bb5
Ielle <kjssd@163.com>
2017-05-12 12:37:34 +0800

    fin

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -10,68 +10,83 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<String> source = Arrays.asList(new String[]{});
+    private List<String> dataList = new ArrayList<>();
     private List<String> province = Arrays.asList(new String[]{"北京","浙江","","","","","","","","",""});
     private List<String> city01 = Arrays.asList(new String[]{"北京市"});
-    private List<String> city02 = Arrays.asList(new String[]{"上海市"});
-    private List<String> city03 = Arrays.asList(new String[]{"天津市"});
-    private List<String> city04 = Arrays.asList(new String[]{"重庆市"});
     private List<String> city17 = Arrays.asList(new String[]{"杭州","温州","嘉兴","宁波","绍兴","台州","湖州","丽水","金华","衢州","舟山"});
+    private List<String> county128 = Arrays.asList(new String[]{"嘉兴","嘉善","海宁","桐乡","平湖","海盐"});
+    private int level=1;
+    private  Button button = null;
+    private  ListView listView = null;
+    private  ArrayAdapter adapter;
+    //选中的省或者直辖市的序号（0,1,2...)
+    private int selectedProvinceIndex=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this,android.R.layout.simple_list_item_1,province);
-        final ListView listView = (ListView) findViewById(R.id.list_view);
+        this.button = (Button) findViewById(R.id.backbutton);
+        listView = (ListView) findViewById(R.id.list_view);
+        this.dataList.addAll(this.province);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, this.dataList);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Toast.makeText(MainActivity.this,province.get(position),Toast.LENGTH_SHORT).show();
-                final Button button = (Button) findViewById(R.id.backbutton);
-
-                if ("北京".equals(province.get(position))){
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                            MainActivity.this,android.R.layout.simple_list_item_1,city01);
-                    listView.setAdapter(adapter);
-                    button.setVisibility(View.VISIBLE);
-                    button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                                    MainActivity.this,android.R.layout.simple_list_item_1,province);
-                            listView.setAdapter(adapter);
-                            button.setVisibility(View.GONE);
-                        }
-                    });
-                }
-                else {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                            MainActivity.this,android.R.layout.simple_list_item_1,city17);
-                    listView.setAdapter(adapter);
-                    button.setVisibility(View.VISIBLE);
-                    button.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                                    MainActivity.this,android.R.layout.simple_list_item_1,province);
-                            listView.setAdapter(adapter);
-                            button.setVisibility(View.GONE);
-                        }
-                    });
+                if (level == 1) {
+                    selectedProvinceIndex = position;
+                    showCities(selectedProvinceIndex);
+                } else if (level == 2) {
+                    showCounties();
+                } else {
+                    //TODO 选定区县，去显示天气界面
                 }
 
-//                Intent intent = new Intent(MainActivity.this,CityActivity.class);
-//                intent.putExtra("province",data[position]);
-//                startActivity(intent);
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(level==3){
+                    showCities(MainActivity.this.selectedProvinceIndex);
+                }else if(level==2){
+                    level=1;
+                    button.setVisibility(View.GONE);
+                    MainActivity.this.dataList.clear();
+                    MainActivity.this.dataList.addAll(province);
+                    MainActivity.this.adapter.notifyDataSetChanged();
+                }
+
+
+
+            }
+        });
+
+    }
+    private void showCities(int position) {
+        level=2;
+        this.button.setVisibility(View.VISIBLE);
+        this.dataList.clear();
+        if("北京".equals(this.province.get(position))){
+            this.dataList.addAll(city01);
+        }else{
+            this.dataList.addAll(city17);
+        }
+        this.adapter.notifyDataSetChanged();
+    }
+
+    private void showCounties() {
+        level=3;
+        this.dataList.clear();
+        this.dataList.addAll(county128);
+        adapter.notifyDataSetChanged();
     }
 }
